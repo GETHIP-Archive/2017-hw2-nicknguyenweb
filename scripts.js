65bcb270b02c38e4992b3ae5f09dff0a1adaada4
nicknguyenweb <nicknguyen41@yahoo.com>
2016-10-01 11:32:25 -0500

    Homework

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  $.getJSON("https://raw.githubusercontent.com/GETHIP/HW2-Template/master/logan.json", function(data){
+  $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-nicknguyenweb/master/nick.json", function(data){
     console.log(data);
     $("#profile-name").text(data.firstName + " " + data.lastName);
     $("#age").text(data.age);
     $("#profile-picture").attr("src", data.picture);
-    $.each(data.nickNames, function(key, value){
-      $("#nicknames").append("<li>" + value.nick + "</li>");
+    $.each(data.qualities, function(key, value){
+      $("#qualities").append("<li>" + value.quality + "</li>");
     });
   });
 });
